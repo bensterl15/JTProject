c19979b8a3a1b22280bd64847f3b68ff62000af0
root <root@localhost.localdomain>
2017-09-21 22:44:52 -0400

    Got the Upper Bar done

--- build-JTProject-Desktop-Debug/ui_mainwindow.h
+++ build-JTProject-Desktop-Debug/ui_mainwindow.h
@@ -14,9 +14,13 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QFrame>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QVBoxLayout>
 
 QT_BEGIN_NAMESPACE
 
@@ -24,8 +28,17 @@ class Ui_MainWindow
 {
 public:
     QFrame *frame;
+    QVBoxLayout *vboxLayout;
+    QFrame *frame1;
+    QHBoxLayout *hboxLayout;
     QFrame *upperFrame;
-    QPushButton *pressme;
+    QHBoxLayout *hboxLayout1;
+    QSpacerItem *horizontalSpacer_2;
+    QPushButton *pushButton;
+    QSpacerItem *horizontalSpacer;
+    QLabel *label;
+    QSpacerItem *horizontalSpacer_3;
+    QSpacerItem *verticalSpacer;
 
     void setupUi(QMainWindow *MainWindow)
     {
@@ -34,12 +47,54 @@ public:
         MainWindow->resize(600, 400);
         frame = new QFrame(MainWindow);
         frame->setObjectName(QStringLiteral("frame"));
-        upperFrame = new QFrame(frame);
+        vboxLayout = new QVBoxLayout(frame);
+        vboxLayout->setSpacing(6);
+        vboxLayout->setContentsMargins(11, 11, 11, 11);
+        vboxLayout->setObjectName(QStringLiteral("vboxLayout"));
+        frame1 = new QFrame(frame);
+        frame1->setObjectName(QStringLiteral("frame1"));
+        hboxLayout = new QHBoxLayout(frame1);
+        hboxLayout->setSpacing(6);
+        hboxLayout->setContentsMargins(11, 11, 11, 11);
+        hboxLayout->setObjectName(QStringLiteral("hboxLayout"));
+        upperFrame = new QFrame(frame1);
         upperFrame->setObjectName(QStringLiteral("upperFrame"));
-        upperFrame->setGeometry(QRect(0, 0, 1000, 50));
-        pressme = new QPushButton(upperFrame);
-        pressme->setObjectName(QStringLiteral("pressme"));
-        pressme->setGeometry(QRect(0, 0, 81, 50));
+        hboxLayout1 = new QHBoxLayout(upperFrame);
+        hboxLayout1->setSpacing(6);
+        hboxLayout1->setContentsMargins(11, 11, 11, 11);
+        hboxLayout1->setObjectName(QStringLiteral("hboxLayout1"));
+        horizontalSpacer_2 = new QSpacerItem(33, 20, QSizePolicy::Fixed, QSizePolicy::Minimum);
+
+        hboxLayout1->addItem(horizontalSpacer_2);
+
+        pushButton = new QPushButton(upperFrame);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+
+        hboxLayout1->addWidget(pushButton);
+
+        horizontalSpacer = new QSpacerItem(137, 10, QSizePolicy::Fixed, QSizePolicy::Minimum);
+
+        hboxLayout1->addItem(horizontalSpacer);
+
+        label = new QLabel(upperFrame);
+        label->setObjectName(QStringLiteral("label"));
+
+        hboxLayout1->addWidget(label);
+
+        horizontalSpacer_3 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        hboxLayout1->addItem(horizontalSpacer_3);
+
+
+        hboxLayout->addWidget(upperFrame);
+
+
+        vboxLayout->addWidget(frame1);
+
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        vboxLayout->addItem(verticalSpacer);
+
         MainWindow->setCentralWidget(frame);
 
         retranslateUi(MainWindow);
@@ -51,7 +106,8 @@ public:
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "JT's Cool Project", Q_NULLPTR));
         upperFrame->setStyleSheet(QApplication::translate("MainWindow", "background-color: white;", Q_NULLPTR));
-        pressme->setText(QApplication::translate("MainWindow", "Play", Q_NULLPTR));
+        pushButton->setText(QApplication::translate("MainWindow", "Play", Q_NULLPTR));
+        label->setText(QApplication::translate("MainWindow", "Label", Q_NULLPTR));
     } // retranslateUi
 
 };
